d6897eb0f6de469bb05a48d9c48f9fc62066d617
anantaw81 <anantaw81@gmail.com>
2022-11-08 07:11:36 +0800

    adding arrow in homepage

--- homepage.php
+++ homepage.php
@@ -6,9 +6,36 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- import bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <style>
+    
+    /* HOMEPAGE */
+
+    .h-navigation {
+      display: flex;
+      margin-right: 8%;
+      margin-left: 8%;
+      justify-content: space-between;
+    }
+
+    .h-navigation-arrow {
+      text-decoration: none;
+      background-color: #e9e9e9;
+      color: black;
+      border-radius: 360px;
+      padding: 5px 10px;
+    }
+
+    .h-navigation-arrow:hover, .h-navigation-arrow:active {
+      background-color: #2196f3;
+      color: white;
+    }
+
+    /* HOMEPAGE END */
+    </style>
 </head>
 <body>
-
     <div class="topnav">
         <a class="active"href="homepage.php">Alkitab</a>
         <a  href="penulis.php">Penulis</a>
@@ -27,7 +54,6 @@
       </div> -->
 
       <main>
-
         <div class="dropdown">
             <label for="membership" >Jelajahi Alkitab :</label>
                 <select name="membership" id="membership">
@@ -48,6 +74,14 @@
                     <option value="silver" selected>Silver</option>
                     <option value="Gold">Gold</option>
                 </select>
+        </div>
+        <div class="h-navigation">
+          <div class="h-navigation-item">
+            <a class="h-navigation-arrow" href=""><i class="bi bi-arrow-left-circle-fill"></i> Prev</a>
+          </div>
+          <div class="h-navigation-item">
+            <a class="h-navigation-arrow" href="">Next <i class="bi bi-arrow-right-circle-fill"></i></a>
+          </div>
         </div>
       </main>
 </body>
